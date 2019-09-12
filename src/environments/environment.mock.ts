1e39209bee111c4c408a4a9ba6d0b13c996477a0
mikef <mstuartflood@gmail.com>
2019-09-12 14:26:03 +0100

    added npm run mock option

---
+++ src/environments/environment.mock.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from './environment.interface';
+
+export const environment: EnvironmentInterface = {
+    baseUrl: '',
+    production: false,
+    tokenKey: 'AUTH_TOKEN_LOCAL'
+};
